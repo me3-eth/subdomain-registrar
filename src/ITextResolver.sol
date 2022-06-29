53cda6d7719cb958f85a9593dad1c34731c6421f
charchar <87212793+0xcharchar@users.noreply.github.com>
2022-06-29 05:43:55 +0100

    fix missing natspec

--- src/ITextResolver.sol
+++ src/ITextResolver.sol
@@ -2,6 +2,9 @@
 pragma solidity 0.8.10;
 
 interface ITextRead {
+    /// @notice Get the text value of a node
+    /// @param node The node which the text records is associated with
+    /// @param key The key which the text is under
     function text(bytes32 node, string calldata key)
         external
         view
@@ -9,8 +12,15 @@ interface ITextRead {
 }
 
 interface ITextWrite {
+    /// @notice Should be emitted when a key's value is updated
+    /// @param node The node which the text records is associated with
+    /// @param key The key which the text is under
     event TextChanged(bytes32 indexed node, string indexed key);
 
+    /// @notice Set the text value of a node
+    /// @param node The node which the text records is associated with
+    /// @param key The key which the text is under
+    /// @param value The value to save for the node and key
     function setText(
         bytes32 node,
         string calldata key,
