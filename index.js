cac16b9d508f5af9fbf4095cd2346a7a6400c5e8
charchar <87212793+0xcharchar@users.noreply.github.com>
2023-01-20 05:25:46 +0000

    prepare for packaging abis

---
+++ index.js
@@ -0,0 +1,9 @@
+const GatewayBeta = require('./out/GatewayBeta.sol/GatewayBeta.json')
+const NftAuthoriser = require('./out/NftAuthoriser.sol/NftAuthoriser.json')
+const Registrar = require('./out/Registrar.sol/Registrar.json')
+
+module.exports = {
+  GatewayBeta,
+  NftAuthoriser,
+  Registrar
+}
