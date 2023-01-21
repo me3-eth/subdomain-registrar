f7284f9e3a64594ced4ed2df024a0f90a3feec15
charchar <87212793+0xcharchar@users.noreply.github.com>
2023-01-21 06:18:47 +0000

    fix: only ship abi

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const NftAuthoriser = require('./out/NftAuthoriser.sol/NftAuthoriser.json')
 const Registrar = require('./out/Registrar.sol/Registrar.json')
 
 module.exports = {
-  GatewayBeta,
-  NftAuthoriser,
-  Registrar
+  GatewayBeta: GatewayBeta.abi,
+  NftAuthoriser: NftAuthoriser.abi,
+  Registrar: Registrar.abi
 }
